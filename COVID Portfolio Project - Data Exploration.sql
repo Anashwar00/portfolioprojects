46611104154e11d89c368906c723a65a6b43774b
Anashwar00 <153188982+Anashwar00@users.noreply.github.com>
2023-12-07 03:42:20 -0800

    Update COVID Portfolio Project - Data Exploration.sql

--- COVID Portfolio Project - Data Exploration.sql
+++ COVID Portfolio Project - Data Exploration.sql
@@ -1,9 +1,7 @@
 /*
 Covid 19 Data Exploration 
 
-Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 
-*/
 
 Select *
 From PortfolioProject..CovidDeaths
@@ -11,7 +9,7 @@ Where continent is not null
 order by 3,4
 
 
--- Select Data that we are going to be starting with
+
 
 Select Location, date, total_cases, new_cases, total_deaths, population
 From PortfolioProject..CovidDeaths
@@ -20,21 +18,21 @@ order by 1,2
 
 
 -- Total Cases vs Total Deaths
--- Shows likelihood of dying if you contract covid in your country
+
 
 Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 From PortfolioProject..CovidDeaths
-Where location like '%states%'
+Where location like '%india%'
 and continent is not null 
 order by 1,2
 
 
 -- Total Cases vs Population
--- Shows what percentage of population infected with Covid
+
 
 Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
+--Where location like '%india%'
 order by 1,2
 
 
@@ -42,7 +40,7 @@ order by 1,2
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
+--Where location like '%india%'
 Group by Location, Population
 order by PercentPopulationInfected desc
 
@@ -51,20 +49,20 @@ order by PercentPopulationInfected desc
 
 Select Location, MAX(cast(Total_deaths as int)) as TotalDeathCount
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
+--Where location like '%india%'
 Where continent is not null 
 Group by Location
 order by TotalDeathCount desc
 
 
 
--- BREAKING THINGS DOWN BY CONTINENT
+
 
 -- Showing contintents with the highest death count per population
 
 Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
+--Where location like '%india%'
 Where continent is not null 
 Group by continent
 order by TotalDeathCount desc
@@ -75,7 +73,7 @@ order by TotalDeathCount desc
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
+--Where location like '%india%'
 where continent is not null 
 --Group By date
 order by 1,2
@@ -83,7 +81,7 @@ order by 1,2
 
 
 -- Total Population vs Vaccinations
--- Shows Percentage of Population that has recieved at least one Covid Vaccine
+
 
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
